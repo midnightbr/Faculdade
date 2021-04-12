d5d4f28a16adfb2386b60b4888066ad3e2dd1185
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-12 07:28:15 -0300

    Java para Iniciantes
    Atributos básicos de java. Declarações e métodos iniciais.

---
+++ java/PrimeiroPrograma/src/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+/**
+ 	Este é um programa Java simples.
+ 	
+ */
+/**
+ * @author Marcos Henrique
+ *
+ */
+class PrimeiroPrograma {
+	// Um programa Java começa com uma chamada a main()
+	public static void main(String args[]) {
+		System.out.println("Java drives the web");
+	}
+}
---
+++ java/SegundoPrograma/src/SegundoPrograma.java
@@ -0,0 +1,22 @@
+/**
+ 	Este cóidigo demonstra uma variável
+ */
+/**
+ * @author Marcos Henrique
+ *
+ */
+class SegundoPrograma {
+	public static void main(String args[]) {
+		int var1; //Esta instrução declara uma variavel
+		int var2; //Esta instrução declara uma outra vari�vel
+		
+		var1 = 1024; //Esta instrução atribui 1024 a var1
+		
+		System.out.println("var1 constains " + var1);
+		
+		var2 = var1 / 2;
+		
+		System.out.println("var2 contains var1 / 2: ");
+		System.out.println(var2);
+	}
+}
